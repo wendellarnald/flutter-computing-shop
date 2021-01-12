2ce6af4f74ac095a35b1155c62c29cd1a9c712b0
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-12 18:12:17 +0000

    Change items in the product

--- lib/models/Product.model.dart
+++ lib/models/Product.model.dart
@@ -19,20 +19,22 @@ class Product {
 List<Product> products = [
   Product(
     id: 1,
-    title: "Mouse",
-    price: 80,
-    size: 5,
-    description: "",
-    image: "",
+    title: "Screen Gaming",
+    price: 250,
+    size: 50,
+    description:
+        "Monitor Gaming AOC 24G2U/BK (24'' - 1 ms - 144 Hz - IPS - FreeSync)",
+    image: "assets/images/Screen.png",
     color: Colors.deepPurple,
   ),
   Product(
     id: 2,
-    title: "Keyboard",
-    price: 110,
-    size: 10,
-    description: "",
-    image: "",
+    title: "Desktop Gaming",
+    price: 1100,
+    size: 100,
+    description:
+        "Desktop Gaming LENOVO IdeaCentre G5 14IMB05 (Intel Core i7-10700 - NVIDIA GeForce GTX 1650 SUPER - RAM: 8 GB - 512 GB SSD PCIe)",
+    image: "assets/images/desktop.png",
     color: Colors.deepOrange,
   ),
 ];
