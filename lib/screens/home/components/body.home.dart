8a64d6ea4f3f2726a74367646eb93aebf5e4ab32
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-18 20:08:41 +0000

    Add categorie class

--- lib/screens/home/components/body.home.dart
+++ lib/screens/home/components/body.home.dart
@@ -1,4 +1,5 @@
 import 'package:computing_shop/constants.dart';
+import 'package:computing_shop/screens/home/components/categories.home.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -21,59 +22,6 @@ class Body extends StatelessWidget {
         ),
         Categories(),
       ],
-    );
-  }
-}
-
-class Categories extends StatefulWidget {
-  @override
-  __CategoriesState createState() => __CategoriesState();
-}
-
-class __CategoriesState extends State<Categories> {
-  List<String> categories = defaultCategories;
-  int selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (context, index) => buildCategory(index),
-      ),
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              categories[index],
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: selectedIndex == index ? textColor : textLightColor,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: defaultPadding / 4),
-              height: 2,
-              width: 30,
-              color: selectedIndex == index ? Colors.black : Colors.transparent,
-            )
-          ],
-        ),
-      ),
     );
   }
 }
