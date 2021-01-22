ba54a5018b94a7a25c70e5ae2ece55f54d57ab5b
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-22 19:11:59 +0000

    Create header in details page

--- lib/screens/details/details.page.dart
+++ lib/screens/details/details.page.dart
@@ -1,5 +1,8 @@
 import 'package:computing_shop/models/Product.model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants.dart';
 
 class DetailsPage extends StatelessWidget {
   final Product product;
@@ -15,6 +18,30 @@ class DetailsPage extends StatelessWidget {
       backgroundColor: product.color,
       appBar: AppBar(
         backgroundColor: product.color,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "asset/icons/back.svg",
+            color: textColor,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/search.svg",
+              color: textColor,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/cart.svg",
+              color: textColor,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
