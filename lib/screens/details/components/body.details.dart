02e6ab262011bae24b0b8654b38466094231f2ba
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-23 20:06:41 +0000

    Structure of the product presentation

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -1,8 +1,21 @@
+import 'package:computing_shop/models/Product.model.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({
+    Key key,
+    this.product,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[],
+      ),
+    );
   }
 }
