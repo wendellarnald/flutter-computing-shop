5dde4ac0d6027634fc1ca6f9d7e438103ed4ea51
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-24 14:46:02 +0000

    create SizeBox for product information

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -1,3 +1,4 @@
+import 'package:computing_shop/constants.dart';
 import 'package:computing_shop/models/Product.model.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,22 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[],
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                ),
       ),
     );
   }
