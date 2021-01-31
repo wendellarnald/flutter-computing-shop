9df3b964198c1f3662596cd1727d88eba1fd1755
Wendell Arnald <wendell.arnald@gmail.com>
2021-01-31 20:37:41 +0000

    Create a new method for product colors

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -89,3 +89,39 @@ class Body extends StatelessWidget {
     );
   }
 }
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorDot({
+    Key key,
+    this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultPadding / 4,
+        right: defaultPadding / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
