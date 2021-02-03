a88e16a66b49a38e188d1be67b63a633635d02de
Wendell Arnald <wendell.arnald@gmail.com>
2021-02-03 21:34:02 +0000

    Send colorSize method for new class

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -1,5 +1,6 @@
 import 'package:computing_shop/constants.dart';
 import 'package:computing_shop/models/Product.model.dart';
+import 'package:computing_shop/screens/details/components/colorSize.details.dart';
 import 'package:computing_shop/screens/details/components/product.details.dart';
 import 'package:flutter/material.dart';
 
@@ -53,95 +54,20 @@ class Body extends StatelessWidget {
   }
 }
 
-class ColorAndSize extends StatelessWidget {
-  const ColorAndSize({
-    Key key,
-    @required this.product,
-  }) : super(key: key);
-
 class Description extends StatelessWidget {
   final Product product;
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Color"),
-              Row(
-                children: <Widget>[
-                  ColorDot(
-                    color: Colors.black,
-                    isSelected: true,
-                  ),
-                  ColorDot(color: Colors.red),
-                  ColorDot(color: Colors.blue),
-                ],
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          child: RichText(
-            text: TextSpan(
-              style: TextStyle(color: textColor),
-              children: [
-                TextSpan(text: "Size\n"),
-                TextSpan(
-                  text: "${product.size} cm",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  final Color color;
-  final bool isSelected;
-
-  const ColorDot({
   const Description({
     Key key,
-    this.color,
-    this.isSelected = false,
     @required this.product,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: defaultPadding / 4,
-        right: defaultPadding / 2,
-      ),
-      padding: EdgeInsets.all(2.5),
-      height: 24,
-      width: 24,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: isSelected ? color : Colors.transparent,
-        ),
     return Padding(
       padding: const EdgeInsets.symmetric(
         vertical: defaultPadding,
       ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: color,
-        ),
       child: Text(
         product.description,
         style: TextStyle(height: 1.5),
