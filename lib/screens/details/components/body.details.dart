c142231bc699cba9822ef8b5e10848b05a403f38
Wendell Arnald <wendell.arnald@gmail.com>
2021-02-04 23:17:53 +0000

    Add to cart

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -46,6 +46,51 @@ class Body extends StatelessWidget {
                       ColorAndSize(product: product),
                       Description(product: product),
                       CounterWithFavBtn(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: defaultPadding),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: defaultPadding),
+                              height: 50,
+                              width: 58,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                border: Border.all(
+                                  color: product.color,
+                                ),
+                              ),
+                              child: IconButton(
+                                icon: SvgPicture.asset(
+                                  "assets/icons/add_to_cart.svg",
+                                  color: product.color,
+                                ),
+                                onPressed: () {},
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                height: 50,
+                                child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                  color: product.color,
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Buy Now".toUpperCase(),
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
