52089dfa949ba1663a65d416b2caae758197480a
Wendell Arnald <wendell.arnald@gmail.com>
2021-02-04 21:56:36 +0000

    Send cart countermethod for new class

--- lib/screens/details/components/cartCounter.details.dart
+++ lib/screens/details/components/cartCounter.details.dart
@@ -1,1 +1,61 @@
+import 'package:computing_shop/constants.dart';
+import 'package:flutter/material.dart';
 
+class CartCounter extends StatefulWidget {
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numberOfItems = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        buildOutLineButton(
+          icon: Icons.remove,
+          press: () {
+            if (numberOfItems > 1) {
+              setState(
+                () {
+                  numberOfItems--;
+                },
+              );
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+          child: Text(
+            numberOfItems.toString().padLeft(2, "0"),
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        buildOutLineButton(
+          icon: Icons.add,
+          press: () {
+            setState(() {
+              numberOfItems++;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  SizedBox buildOutLineButton({IconData icon, Function press}) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlineButton(
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(13),
+        ),
+        onPressed: press,
+        child: Icon(icon),
+      ),
+    );
+  }
+}
