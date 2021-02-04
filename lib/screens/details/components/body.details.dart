6851c785e2465c2e948b28409cfbe259f088a597
Wendell Arnald <wendell.arnald@gmail.com>
2021-02-04 22:08:38 +0000

    Send colorSize method for new class

--- lib/screens/details/components/body.details.dart
+++ lib/screens/details/components/body.details.dart
@@ -5,6 +5,7 @@ import 'package:computing_shop/screens/details/components/colorSize.details.dart
 import 'package:computing_shop/screens/details/components/description.details.dart';
 import 'package:computing_shop/screens/details/components/product.details.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -43,7 +44,7 @@ class Body extends StatelessWidget {
                     children: <Widget>[
                       ColorAndSize(product: product),
                       Description(product: product),
-                      CartCounter(),
+                      CounterWithFavBtn(),
                     ],
                   ),
                 ),
@@ -53,6 +54,32 @@ class Body extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CounterWithFavBtn extends StatelessWidget {
+  const CounterWithFavBtn({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CartCounter(),
+        Container(
+          padding: EdgeInsets.all(8),
+          height: 32,
+          width: 32,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            shape: BoxShape.circle,
+          ),
+          child: SvgPicture.asset("assets/icons/heart.svg"),
+        ),
+      ],
     );
   }
 }
